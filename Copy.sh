ac380e6539b4fa4a783c789466ad417bca47fa2d
Zukunter <Zukunter@localhost.localdomain>
2026-04-20 20:33:09 -0300

    Adding SubModule For Antidote

--- Copy.sh
+++ Copy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+sudo zypper in git hyprland zsh hyprshot hyprsunset zig rust clang neovim waybar \ 
+  fuzzel mako ydotool wtype flatpak lf curl
 # Make the Directorys
 mkdir -p ~/.local/share/fonts
+
+mkdir -p ~/User/ElBastionDelOlvido
+mkdir -p ~/User/Fixed/Configurations
+mkdir -p ~/User/Fixed/Downloads
+mkdir -p ~/User/Fixed/Pictures
+mkdir -p ~/User/Fixed/Videos
+mkdir -p ~/User/Fixed/Musics
+
 mkdir -p ~/Temporal
 
 echo "Downloading Fonts"
@@ -10,4 +20,6 @@ unzip -o ~/Temporal/JetBrainsMono.zip -d ~/.local/share/fonts
 
 fc-cache -fv
 mv ~/.zshenv ~/.zshenv.bank
-curl -L https://github.com/Zukunter/ElBastionDelOlvido/SetEnv.zsh -o ~/.zshenv
+curl -L https://raw.githubusercontent.com/Zukunter/ElBastionDelOlvido/main/SetEnv.zsh -o ~/.zshenv
+
+git clone https://github.com/Zukunter/ElBastionDelOlvido.git ~/Hola
