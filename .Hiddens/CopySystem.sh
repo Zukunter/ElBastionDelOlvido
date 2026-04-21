e751afe6e0f1dce7a20d8e8b6f0de48a0cf7a159
Zukunter <Zukunter@localhost.localdomain>
2026-04-21 16:14:27 -0300

    Adding SubModule For Antidote

--- .Hiddens/CopySystem.sh
+++ .Hiddens/CopySystem.sh
@@ -2,7 +2,6 @@
 sudo zypper in git hyprland zsh hyprshot hyprsunset zig rust clang neovim waybar \ 
   fuzzel mako ydotool wtype flatpak lf curl
 # Make the Directorys
-mkdir -p ~/.local/share/fonts
 
 mkdir -p ~/User/ElBastionDelOlvido
 mkdir -p ~/User/Fixed/Configurations
@@ -13,12 +12,14 @@ mkdir -p ~/User/Fixed/Musics
 
 mkdir -p ~/Temporal
 
-echo "Downloading Fonts"
+
+# Install JetBrainsMono
+mkdir -p ~/.local/share/fonts
 curl -L https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip -o ~/Temporal/JetBrainsMono.zip
-
 unzip -o ~/Temporal/JetBrainsMono.zip -d ~/.local/share/fonts
-
 fc-cache -fv
+
+# Set Variables
 mv ~/.zshenv ~/.zshenv.bank
 curl -L https://raw.githubusercontent.com/Zukunter/ElBastionDelOlvido/main/SetEnv.zsh -o ~/.zshenv
 
