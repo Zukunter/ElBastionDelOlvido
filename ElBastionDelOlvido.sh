126a5167f2d21a6becbba8b3b9b60af7fa99ba69
Zukunter <Zukunter@localhost.localdomain>
2026-05-05 18:00:34 -0300

    Updating installer

--- ElBastionDelOlvido.sh
+++ ElBastionDelOlvido.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 # Make the Directorys
-mkdir -p ~/User/ElBastionDelOlvido
-mkdir -p ~/User/Fixed/Configurations
-mkdir -p ~/User/Fixed/Downloads
-mkdir -p ~/User/Fixed/Pictures
-mkdir -p ~/User/Fixed/Videos
-mkdir -p ~/User/Fixed/Audios
-mkdir -p ~/WorkSpace
+#mkdir -p $HOME/User/ElBastionDelOlvido
+mkdir -p $HOME/User/Fixed/Configurations
+mkdir -p $HOME/User/Fixed/Downloads
+mkdir -p $HOME/User/Fixed/Pictures
+mkdir -p $HOME/User/Fixed/Videos
+mkdir -p $HOME/User/Fixed/Audios
+mkdir -p $HOME/WorkSpace
 
 
-Ghostty=~/User/Fixed/Configurations/ghostty
+Ghostty=$HOME/User/Fixed/Configurations/ghostty
 mkdir "$Ghostty"
 touch "$Ghostty/config"
-echo "config-file = ~/User/ElBastionDelOlvido/Ghostty.toml" > "$Ghostty/config"
+echo "config-file = $HOME/User/ElBastionDelOlvido/Ghostty.toml" > "$Ghostty/config"
 
-Hyprland=~/User/Fixed/Configurations/hypr
+Hyprland=$HOME/User/Fixed/Configurations/hypr
 mkdir "$Hyprland"
 touch "$Hyprland/hyprland.conf"
-echo "source = ~/User/ElBastionDelOlvido/Desktop/Hyprland/Start.conf" > "$Hyprland/hyprland.conf"
+echo "source = $HOME/User/ElBastionDelOlvido/Desktop/Hyprland/Start.conf" > "$Hyprland/hyprland.conf"
 # Copy the repository
-git clone https://github.com/Zukunter/ElBastionDelOlvido/Stable ~/User/ElBastionDelOlvido
+git clone -b Stable https://github.com/Zukunter/ElBastionDelOlvido.git ~/User/ElBastionDelOlvido
 # Install JetBrainsMono
-mkdir -p ~/.local/share/fonts
-mkdir ~/Temporal
-curl -L https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip -o ~/Temporal/JetBrainsMono.zip
-unzip -o ~/Temporal/JetBrainsMono.zip -d ~/.local/share/fonts
+mkdir -p $HOME/.local/share/fonts
+mkdir $HOME/Temporal
+curl -L https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip -o $HOME/Temporal/JetBrainsMono.zip
+unzip -o $HOME/Temporal/JetBrainsMono.zip -d ~/.local/share/fonts
 fc-cache -fv
 # Set Variables
-curl -L https://raw.githubusercontent.com/Zukunter/ElBastionDelOlvido/Stable/Env.sh -o ~/.zshenv
+curl -L https://raw.githubusercontent.com/Zukunter/ElBastionDelOlvido/Stable/Env.sh -o $HOME/.zshenv
