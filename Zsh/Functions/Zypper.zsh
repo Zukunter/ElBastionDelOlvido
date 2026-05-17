5739f98aa2634eeb4e1a7ae9347984da7476b5f0
Zukunter <Zukunter@localhost.localdomain>
2026-05-17 11:26:06 -0300

    3.3.0 Final bash installer

--- Zsh/Functions/Zypper.zsh
+++ Zsh/Functions/Zypper.zsh
@@ -13,3 +13,9 @@ function zydup()
 function zysr(){
     zypper search "$@"
 }
+function snapperClean() {
+    for SNAP in $(sudo snapper list | awk 'NR>2 {print $1,$8}' | grep -v "yes" | awk '{print $1}'); do
+        sudo snapper delete $SNAP
+    done
+}
+
